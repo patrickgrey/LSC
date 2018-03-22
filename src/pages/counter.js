0d238678b64cf6c50da5ede145fce5550408b702
Patrick Grey <websemantic@yahoo.co.uk>
2018-03-22 23:23:31 +0000

    Starting tutorial.

---
+++ src/pages/counter.js
@@ -0,0 +1,13 @@
+
+
+
+
+import React from "react";
+
+class Counter extends React.Component {
+  render() {
+    return <div>Hello Class Component</div>;
+  }
+}
+
+export default Counter;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <h1>Hi peeps</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>
+      <Link to="/counter/">Counter</Link>
+    </p>
+    <p>
+      <Link to="/page-2/">Page 2</Link>
+    </p>
   </div>
 )
 
