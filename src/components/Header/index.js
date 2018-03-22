25ac69760644251c8e3a6dc52393d86682317bcd
Patrick Grey <websemantic@yahoo.co.uk>
2018-03-22 20:40:58 +0000

    Change title.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -23,7 +23,7 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          Lochgilphead Soccer Centre
         </Link>
       </h1>
     </div>
